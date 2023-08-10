63d8fda79aac228d8cce6b46ce166445128d80a9
TetianaTkalich <114866505+TetianaTkalich@users.noreply.github.com>
2023-08-10 19:28:58 +0300

    HW06

---
+++ controllers/auth.js
@@ -0,0 +1,219 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
+const { v4: uuidv4 } = require("uuid");
+
+const { User } = require("../models/user");
+
+const { HttpError, sendEmail } = require("../helpers");
+
+const { schemas } = require("../models/user");
+
+const { SECRET_KEY, BASE_URL } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
+
+const register = async (req, res, next) => {
+  try {
+    const { error } = schemas.registerSchema.validate(req.body);
+
+    if (error) {
+      throw HttpError(400, "Missing required name field");
+    }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      throw HttpError(409, "Email in use");
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
+    const verificationToken = uuidv4();
+
+    const newUser = await User.create({
+      ...req.body,
+      password: hashPassword,
+      avatarURL,
+      verificationToken,
+    });
+
+    const verifyEmail = {
+      to: email,
+      subject: "Verify email",
+      html: `<a tarhet="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Click verify email</a>`,
+    };
+
+    await sendEmail(verifyEmail);
+
+    res.status(201).json({ email: newUser.email, subscription: "starter" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const verifyEmail = async (req, res, next) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({ verificationToken });
+
+    if (!user) {
+      throw HttpError(404, "User not found");
+    }
+
+    await User.findByIdAndUpdate(user._id, {
+      verify: true,
+      verificationToken: null,
+    });
+
+    res.json({ message: "Verification successful" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const resendVerifyEmail = async (req, res, next) => {
+  try {
+    const { error } = schemas.emailSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "Missing required name field");
+    }
+
+    const { email } = req.body;
+    const user = User.findOne({ email });
+
+    if (!user) {
+      throw HttpError(404, "User not found");
+    }
+
+    if (user.verify) {
+      throw HttpError(400, "Verification has already been passed");
+    }
+
+    const verifyEmail = {
+      to: email,
+      subject: "Verify email",
+      html: `<a tarhet="_blank" href="${BASE_URL}/api/users/verify/${user.verificationToken}">Click verify email</a>`,
+    };
+
+    await sendEmail(verifyEmail);
+
+    res.json({ message: "Verification email sent" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    const { error } = schemas.loginSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "Missing required name field");
+    }
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw HttpError(401, "Email or password is wrong");
+    }
+
+    if (!user.verify) {
+      throw HttpError(401, "Email not verified");
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      throw HttpError(401, "Email or password is wrong");
+    }
+
+    const payload = { id: user._id };
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+    await User.findByIdAndUpdate(user._id, { token });
+
+    res.json({ token, user: { email, subscription: user.subscription } });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+
+  await User.findByIdAndUpdate(_id, { token: null });
+
+  res.status(204).json();
+};
+
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+
+  res.json({ email, subscription });
+};
+
+const updateSubscriptionUser = async (req, res, next) => {
+  try {
+    const { error } = schemas.updateSubscriptionSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "Missing field subscription");
+    }
+
+    const { subscription } = req.body;
+    if (!["starter", "pro", "business"].includes(subscription)) {
+      throw HttpError(400, "Missing field subscription");
+    }
+
+    const { id } = req.params;
+    const result = await User.findByIdAndUpdate(
+      id,
+      { subscription },
+      { new: true }
+    );
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateAvatar = async (req, res, next) => {
+  try {
+    const { _id } = req.user;
+    const { path: tmpUpload, originalname } = req.file;
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+
+    await Jimp.read(tmpUpload)
+      .then((image) => {
+        return image.resize(250, 250).write(resultUpload);
+      })
+      .catch((error) => {
+        throw error;
+      });
+
+    await fs.rename(tmpUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+
+    res.json({ avatarURL });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  register,
+  verifyEmail,
+  resendVerifyEmail,
+  login,
+  logout,
+  getCurrent,
+  updateSubscriptionUser,
+  updateAvatar,
+};
